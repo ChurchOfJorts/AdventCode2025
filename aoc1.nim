2c8fcd26018b8ace09935dbce4593c725f61f89c
Morgen Nicodemus <reidnicodemus@gmail.com>
2025-12-01 13:18:28 -0600

    Input Processing

--- aoc1.nim
+++ aoc1.nim
@@ -1,12 +1,18 @@
 #Advent Of Code Problem 1
+import std/strutils
 
-proc processInputFile() = 
+proc moveDial(dir: char, turns: int) = 
+    echo dir, " ", turns
+
+proc decodeInputFile() = 
     for line in lines "aoc1input.txt":
-        echo line
+        var dir: char = line[0]
+        var turns: int = parseInt(line[1..^1])
+        moveDial(dir, turns)
 
 proc main() =
   const START_NUM = 50
-  processInputFile()
+  decodeInputFile()
 
 when isMainModule:
   main()
