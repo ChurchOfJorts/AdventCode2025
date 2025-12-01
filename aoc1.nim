fbebd7d4f4679f4fd5024e38efe7b8d70a030eb2
Morgen Nicodemus <reidnicodemus@gmail.com>
2025-12-01 15:59:51 -0600

    algorithms :(

--- aoc1.nim
+++ aoc1.nim
@@ -2,17 +2,22 @@
 import std/strutils
 
 proc moveDial(dir: char, turns: int) = 
-    echo dir, " ", turns
+    echo turns
+    if dir == 'R':
+        echo 'R', (dialNum + turns)
+    elif dir == 'L':
+        echo 'L', (dialNum - turns)
+    else:
+        echo "error"
 
-proc decodeInputFile() = 
+proc main() =
+    var dialNum: int = 50
+
+    #Process the input line by line
     for line in lines "aoc1input.txt":
         var dir: char = line[0]
         var turns: int = parseInt(line[1..^1])
         moveDial(dir, turns)
-
-proc main() =
-  const START_NUM = 50
-  decodeInputFile()
 
 when isMainModule:
   main()
