5c37d80efe8293c3a31341ead26c423a8106af87
Morgen Nicodemus <reidnicodemus@gmail.com>
2025-12-02 15:38:53 -0600

    1 solved :)

--- aoc1.nim
+++ aoc1.nim
@@ -1,23 +1,30 @@
 #Advent Of Code Problem 1
 import std/strutils
 
-proc moveDial(dir: char, turns: int) = 
-    echo turns
+proc moveDial(dialNum: int, dir: char, turns: int): int = 
+    result = dialNum
     if dir == 'R':
-        echo 'R', (dialNum + turns)
+        result = ((((dialNum + turns) mod 100) + 100) mod 100)
+        echo "Move ",turns," right to ", result
     elif dir == 'L':
-        echo 'L', (dialNum - turns)
+        result = ((((dialNum - turns) mod 100) + 100) mod 100)
+        echo "Move ",turns," left to ", result
     else:
         echo "error"
+    return result
 
 proc main() =
     var dialNum: int = 50
-
+    var zeroCounter: int = 0
+    echo "Starting: ", dialNum
     #Process the input line by line
     for line in lines "aoc1input.txt":
         var dir: char = line[0]
         var turns: int = parseInt(line[1..^1])
-        moveDial(dir, turns)
+        dialNum = moveDial(dialNum, dir, turns)
+        if (dialNum == 0):
+            zeroCounter = zeroCounter + 1
+        echo "Zeros: ", zeroCounter
 
 when isMainModule:
-  main()
+    main()
